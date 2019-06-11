94a1e9ea9a22d6bee7c83a0ac1396560cede78d9
Charles F. Gammie <gammie@illinois.edu>
2019-06-11 10:23:56 -0500

    added some comments to init.c; fixed bug in makefile

--- prob:orszag_tang/init.c
+++ prob:orszag_tang/init.c
@@ -3,8 +3,19 @@
 
 initial conditions for relativistic orszag-tang vortex.
 
-description follows initial conditions for the athena
-	test problem
+relativistic version of the Orszag-Tang vortex
+Orszag & Tang 1979, JFM 90, 129-143.
+original OT problem was incompressible
+this is based on compressible version given
+in Toth 2000, JCP 161, 605.
+
+in the limit tscale -> 0 the problem is identical
+to the nonrelativistic problem; as tscale increases
+the problem becomes increasingly relativistic 
+
+grid is fixed in coordinates.c, runs from
+-\pi < x1 <= \pi
+-\pi < x2 <= \pi
 
 */
 
@@ -33,8 +44,8 @@ void init()
 
 	/* output frequencies */
 	DTd = tf/10.;		/* dumping frequency, in units of M */
-	DTl = tf/200.;		/* logfile frequency, in units of M */
-	DTi = tf/200.;		/* image file frequ., in units of M */
+	DTl = tf/100.;		/* logfile frequency, in units of M */
+	DTi = tf/100.;		/* image file frequ., in units of M */
 	DTr = 512;		/* restart file frequ., in timesteps */
 
 	/* start diagnostic counters */
@@ -43,16 +54,26 @@ void init()
 	image_cnt = 0;
 	rdump_cnt = 0;
 
-	/* relativistic Orszag-Tang vortex */
+	/* relativistic version of the Orszag-Tang vortex 
+	  Orszag & Tang 1979, JFM 90, 129-143. 
+	  original OT problem was incompressible 
+	  this is based on compressible version given
+	  in Toth 2000, JCP 161, 605.
+
+	  in the limit tscale -> 0 the problem is identical
+	  to the nonrelativistic problem; as tscale increases
+	  the problem becomes increasingly relativistic */
+
+	double phase = M_PI ;   /* puts current sheet in box middle */
 	ZLOOP {
 		coord(i, j, CENT, X);
 
-                p[i][j][U1] = -sin(X[2] + M_PI);
-                p[i][j][U2] =  sin(X[1] + M_PI) ;
+                p[i][j][U1] = -sin(X[2] + phase);
+                p[i][j][U2] =  sin(X[1] + phase) ;
                 p[i][j][U3] = 0.;
 
-                p[i][j][B1] = -sin(X[2] + M_PI);
-                p[i][j][B2] =  sin(2.*(X[1] + M_PI));
+                p[i][j][B1] = -sin(X[2] + phase);
+                p[i][j][B2] =  sin(2.*(X[1] + phase));
                 p[i][j][B3] = 0. ;
 
                 p[i][j][RHO] = 25./9.;
